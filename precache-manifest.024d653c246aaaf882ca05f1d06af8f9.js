a8cf6167f34de46b5f189ec167bf30ee2996b637
Vidya Sagar Jha <kingvsj@gmail.com>
2020-08-23 15:57:37 +0530

    Updates

---
+++ precache-manifest.024d653c246aaaf882ca05f1d06af8f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cebb538989095186077c6f125baf3da9",
+    "url": "/BMI_Calculator_React/index.html"
+  },
+  {
+    "revision": "bd6316f46c593bdda13b",
+    "url": "/BMI_Calculator_React/static/js/2.361a5bcf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BMI_Calculator_React/static/js/2.361a5bcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34f5fe18c2deb9de9e33",
+    "url": "/BMI_Calculator_React/static/js/main.c65b7273.chunk.js"
+  },
+  {
+    "revision": "e2e3e66da5b64c14ebbf",
+    "url": "/BMI_Calculator_React/static/js/runtime-main.6071a8d6.js"
+  }
+]);
